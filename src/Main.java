ea3eb3e05d52d0b31dbc2b980f8c8bb2ea4c881a
ipetryashkin <ipetryashkin@yandex.ru>
2024-06-18 18:05:22 +0300

    ДЗ к Леции 12

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,43 @@
 public class Main {
-
     public static void main(String[] args) {
         String[][] arr = {
                 {"1", "2", "3","4"},
                 {"5", "6", "7", "8"},
                 {"9", "10", "11", "12"},
-                {"13", "14", "15", "sixteen"}
+                {"13", "14", "15", "16"}
         };
 
         try {
-            int sum = SizeAndSumArray.sizeAndSumArray(arr);
+            int sum = sizeAndSumArray(arr);
             System.out.println("Сумма элементов массива: " + sum);
         } catch (MyArraySizeException e) {
             System.out.println(e.getMessage());
         } catch (MyArrayDataException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static int sizeAndSumArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Неверный размер массива! Размер массива должен быть 4x4!");
+        }
+
+        int sum = 0;
+
+        for (int rowIndex = 0; rowIndex < arr.length; rowIndex++) {
+            if (arr[rowIndex].length != 4) {
+                throw new MyArraySizeException("Неверный размер массива! Размер массива должен быть 4x4!");
+            }
+
+            for (int columnIndex = 0; columnIndex < arr[rowIndex].length; columnIndex++) {
+                try {
+                    sum += Integer.parseInt(arr[rowIndex][columnIndex]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + (rowIndex + 1) + "][" + (columnIndex + 1) + "]");
+                }
+            }
+        }
+
+        return sum;
     }
 }
